255877c0edd00e89d859e32156c1591f583d9355
jesus0104 <jesus0104@gmail.com>
2019-10-26 11:44:05 -0500

    Quinto Comentario

--- ddl_tablas.sql
+++ ddl_tablas.sql
@@ -1,9 +1,12 @@
 -- creacion de tablas SQL
 -- autor: Jesus Ramirez
+-- Trabajando en Rama Alterna
 
 create table td_cliente (
 nombre varchar2(50),
  apellidos varchar2(50)
  direccion varchar2(70)
  )
+ tablespace tmp;
+ 
  
